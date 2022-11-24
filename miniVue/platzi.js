57ba82e64b368ed4aa0f7b9efff39dc0eb42627e
Irwin <ijesperanza@salud.gob.sv>
2022-11-24 12:35:56 -0600

    mODIFICACION

--- miniVue/platzi.js
+++ miniVue/platzi.js
@@ -1,20 +1,27 @@
-class PlatziReactive{
-    constructor(options){
-        this.origin = options.data()
+class PlatziReactive {
+    /*
+      options:
+        data() => { ... }
+    */
+    constructor({ data }) {
+      this.origen = data();
     }
-mount(){
-    document.querySelectorAll('*[p-text]').forEach(el=>{
-        this.pText(el,this.origin,el.getAttribute('p-text'))
-    })
-}
-
-    pText(){}
-
-    pModel(){}
-}
-
-var platzi ={
-    createApp(options){
-        return new PlatziReactive(options)
+  
+    mount() {
+      document.querySelectorAll("*[p-text]").forEach(el => {
+        this.pText(el, this.origen, el.getAttribute("p-text"));
+      });
     }
-}
+  
+    pText(el, target, name) {
+      el.innerText = target[name];
+    }
+  
+    pModel() {}
+  }
+  
+  var Platzi = {
+    createApp(options) {
+      return new PlatziReactive(options);
+    }
+  };
